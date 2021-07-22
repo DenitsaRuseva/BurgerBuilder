bd0697d61654c418953c776e86dc0aacc5c14afb
DenicaRuseva <drr_87@abv.bg>
2021-07-22 09:28:49 +0100

    fixing firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -32,7 +32,7 @@ class BurgerBuilder extends Component {
 
     componentDidMount () {
         console.log(this.props);
-        axios.get( 'https://react-my-burger.firebaseio.com/ingredients.json' )
+        axios.get( 'https://burger-a0acb-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json/' )
             .then( response => {
                 this.setState( { ingredients: response.data } );
             } )
