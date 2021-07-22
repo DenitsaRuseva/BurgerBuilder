863dfb5808e88f29ddc07052574b63f9c633715f
DenicaRuseva <drr_87@abv.bg>
2021-07-22 09:36:36 +0100

    fix orders

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://burger.firebaseio.com/'
+    baseURL: 'https://burger-a0acb-default-rtdb.europe-west1.firebasedatabase.app/'
 });
 
 export default instance;
